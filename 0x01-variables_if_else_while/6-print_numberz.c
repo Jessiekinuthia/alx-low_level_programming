645845566a0550db15aeb75b81861aa60af2dc70
Jessiekinuthia <59712553+Jessiekinuthia@users.noreply.github.com>
2023-02-18 05:36:25 -0800

    waah

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10
+ * Return: 0
+*/
+int main(void)
+{
+	int numb = 48;
+
+	while (numb <= 57)
+	{
+		putchar(numb);
+		numb++;
+	}
+	putchar('\n');
+	return (0);
+}
