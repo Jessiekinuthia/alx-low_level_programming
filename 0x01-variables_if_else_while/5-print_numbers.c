ad27ec36927fc809429244f5cf9ef8170e2a6804
Jessiekinuthia <59712553+Jessiekinuthia@users.noreply.github.com>
2023-02-18 03:26:08 -0800

    another one

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10
+ * Return: 0
+*/
+int main(void)
+{
+	int numb = 0;
+
+	while (numb <= 9)
+	{
+		printf("%d", numb);
+		numb++;
+	}
+	return (0);
+}
