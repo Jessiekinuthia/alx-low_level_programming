aa20b6ed5c27798f47fc3eb7903e464af48098c1
Jessiekinuthia <59712553+Jessiekinuthia@users.noreply.github.com>
2023-02-18 06:47:30 -0800

    kasongo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int numb = 48;
+
+	while (numb <= 57)
+	{
+		putchar(numb);
+		putchar(',');
+		putchar(' ');
+		numb++;
+	}
+	putchar('\n');
+	return (0);
+
+
+}
