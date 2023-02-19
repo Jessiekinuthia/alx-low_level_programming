6438cff3e36a114df7d0a791d780b96cf435dfe4
Jessiekinuthia <59712553+Jessiekinuthia@users.noreply.github.com>
2023-02-19 11:23:51 -0800

    Another

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 /**
- * main - Main block
- * Return: Always 0
+ * main - Prints double digits
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (num1 = 0; num1 < 100; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (num2 = 0; num2 < 100; num2++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 == 98 && num2 == 99)
-			continue;
-			putchar(',');
-			putchar(' ');
-			putchar('\n');
-			return (0);
+			if (num2 > num1)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
+	putchar('\n');
+	return (0);
 }
