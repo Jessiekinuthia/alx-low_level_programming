315474951463d46059e33e06a2f814532a88f860
Jessiekinuthia <59712553+Jessiekinuthia@users.noreply.github.com>
2023-02-21 10:22:10 -0800

    prints special characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: lowercase character
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
