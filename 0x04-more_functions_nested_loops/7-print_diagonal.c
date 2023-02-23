39fb2b65282a204f0327a70c1e1cd6bfeb79cf00
Jessiekinuthia <59712553+Jessiekinuthia@users.noreply.github.com>
2023-02-23 12:00:58 -0800

    prints

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,7 @@ void print_diagonal(int n)
 		_putchar('\n');
 	for (m = 1; m <= n; m++)
 	{
-		for (space = 0; space < m; space++)
+		for (space = 1; space < m; space++)
 			_putchar(32);
 		_putchar(92);
 		_putchar('\n');
