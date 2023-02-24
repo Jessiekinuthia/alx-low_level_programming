19cb198217b2b13749a399143b816d2897157bad
Jessiekinuthia <59712553+Jessiekinuthia@users.noreply.github.com>
2023-02-24 12:50:21 -0800

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - debugging
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	postitive_or_negative(0);
+	postitive_or_negative(2);
+	postitive_or_negative(-8);
+	return (0);
+}
